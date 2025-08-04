96a296558fa202d30d9db9d3aebb699dd83d4342
AzaradoChan <65982758+vitorpsogarcia@users.noreply.github.com>
2025-08-04 20:53:18 -0300

    [ FIX ] Remoção da classe e endpoints calculadora

--- backend/src/main/java/com/leilao/backend/controller/Hello.java
+++ backend/src/main/java/com/leilao/backend/controller/Hello.java
@@ -1,35 +1,14 @@
 package com.leilao.backend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.leilao.backend.model.Calculadora;
-import com.leilao.backend.service.HelloService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
-
-    @Autowired
-    private HelloService helloService;
 
     @GetMapping("/")
     public String hello() {
         return "Olá, Spring";
     }
-
-    @GetMapping("/somar")
-    public Integer somar(@RequestParam("v1") Integer valor1,
-            @RequestParam("v2") Integer valor2) {
-        return valor1 + valor2;
-    }
-
-    @PostMapping("/somar")
-    public Calculadora somar(@RequestBody Calculadora calculadora) {
-        return helloService.somar(calculadora);
-    }
-
 }
