86b7f1f4e776e09a3413e402cfd0d5a0f63711ee
AzaradoChan <65982758+vitorpsogarcia@users.noreply.github.com>
2025-08-04 21:41:01 -0300

    [ FEAT ] Adição de campos em Perfil

--- backend/src/main/java/com/leilao/backend/model/Perfil.java
+++ backend/src/main/java/com/leilao/backend/model/Perfil.java
@@ -4,7 +4,11 @@ import com.leilao.backend.enums.TipoPerfil;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
 
 @Entity
 @Data
@@ -15,10 +19,27 @@ public class Perfil {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(length = 50)
+    private String nome;
+
     @NotBlank(message = "{validation.tipo.notblank}")
     @Column(name = "tipo_perfil")
     @Enumerated(EnumType.STRING)
     private TipoPerfil tipo;
+
+    @Column(name="criado_em")
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date criadoEm;
+
+    @Column(name="atualizado_em", nullable = true)
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date atualizadoEm;
+
+    @Column(name="excluido_em", nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date excluidoEm;
 
     public Perfil atualizaValores(Perfil perfilAtualizado) {
         this.setTipo(perfilAtualizado.getTipo());
